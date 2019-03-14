3ba02958619285a594da1fbd2920774af4ca52d9
zq <1083518075@qq.com>
2019-03-14 09:37:31 +0800

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <div class="banner">
+    <div>
+      <img src="../assets/channel_logo.png" alt="">
+      <button>菜单</button>
+    </div>
+    <h1>青牛视频服务</h1>
+    <h3>面向金融行业打造在线视频营销平台</h3>
+    <div>
+
+    </div>
+  </div>
+</div>
+
+
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+    name:'Index',
+    data () {
+      return {
+
+      }
+    }
+  }
+
+</script>
+
+<style lang="stylus">
+.banner
+  width: 100%
+  height: 6.95rem
+  background:url("../assets/banner_bg.jpg") no-repeat center
+  background-size:cover
+</style>
